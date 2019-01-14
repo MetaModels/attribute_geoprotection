8c3d6fe1f85f051479b97f87fa3a72de21d8aa85
Mini Model <minimodel@metamodel.me>
2019-01-14 14:22:33 +0100

    Updated translations from transifex.

---
+++ contao/languages/et/default.php
@@ -0,0 +1,9 @@
+<?php
+/**
+
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['gp_hide'] = 'Peida';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['gp_show'] = 'Näita';
+
---
+++ contao/languages/et/tl_metamodel_attribute.php
@@ -0,0 +1,9 @@
+<?php
+/**
+
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']     = 'Vaate seaded';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['geoprotection']['0'] = 'Riigid';
+
